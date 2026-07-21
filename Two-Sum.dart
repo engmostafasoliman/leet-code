977dd500028791ce5c5acfbdec4e07b39ef746ed
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-07-21 12:16:24 +0400

    Implement twoSum function in Two-Sum.dart

---
+++ Two-Sum.dart
@@ -0,0 +1,15 @@
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+      final Map<int, int> seen = {}; 
+  for (int i = 0; i < nums.length; i++) {
+    final int complement = target - nums[i];
+
+    if (seen.containsKey(complement)) {
+      return [seen[complement]!, i];
+    }
+    seen[nums[i]] = i;
+  }
+
+  return []; 
+  }
+}

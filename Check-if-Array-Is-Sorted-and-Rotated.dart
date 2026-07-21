5b31af7b1ef35236f6c223dfa2f5c23c47356e5b
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-07-21 12:04:27 +0400

    Implement check for sorted and rotated array

---
+++ Check-if-Array-Is-Sorted-and-Rotated.dart
@@ -0,0 +1,13 @@
+class Solution {
+  bool check(List<int> nums) {
+    int n = nums.length;
+    int drops = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            drops++;
+            if (drops > 1) return false;
+        }
+    }
+    return true;
+  }
+}

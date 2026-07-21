434cb9aca679866f1ceb76dc69d0d15007547b6e
Mostafa Soliman <engmostafasoliman0@gmail.com>
2026-07-21 12:10:13 +0400

    Implement removeDuplicates function in Dart

---
+++ Remove-Duplicates-from-Sorted-Array.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int removeDuplicates(List<int> nums) {
+  if (nums.isEmpty) return 0;
+// first element is always unique
+  int k = 1; 
+  for (int i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[k - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+  }
+}
